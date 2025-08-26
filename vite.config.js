8cf198733311390394357d297f4dc87c17c98ef2
Mathi200204 <arulkumaranmathiyalakan29@gmail.com>
2025-08-26 11:52:44 +0530

    change in hero.jsx file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio-Main/",   
+  base: "/Portfolio-Main",   
   plugins: [react()],
   resolve: {
     alias: {
