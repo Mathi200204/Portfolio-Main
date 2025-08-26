bb6358f28ce6816d2bead4778c0108f628b61916
Mathi200204 <arulkumaranmathiyalakan29@gmail.com>
2025-08-26 08:46:23 +0530

    change in package json

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Portfolio-Main/",   
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "EVAL") return;
+        warn(warning);
+      },
     },
   },
 });
